37e67e3aebad4300a7339e0717d58572adb9a1b4
Cássio Zen <cassiozen@gmail.com>
2018-03-22 11:25:36 -0400

    typo

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import Galery from "./Galery";
+import Gallery from "./Gallery";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Galery title="Lakes">
+        <Gallery title="Lakes">
           <img src="/images/canoeing.jpg" alt="Canoeing lake" />
           <img src="/images/montains.jpg" alt="Between Montains" />
           <img src="/images/frozen.jpg" alt="Frozen lake" />
-        </Galery>
+        </Gallery>
       </div>
     )
   }
